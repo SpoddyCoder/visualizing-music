71ab7a68d4b6afb9acda1b9238c85bc5483cb457
SpoddyCoder <paul@spoddycoder.com>
2024-03-31 03:20:51 +0100

    Update paths

--- lucid-sonic-dreams/drumwah/drumwah-lsd.py
+++ lucid-sonic-dreams/drumwah/drumwah-lsd.py
@@ -1,21 +1,21 @@
 from lucidsonicdreams import LucidSonicDream
 
-L = LucidSonicDream(song = '../ai-music-viz/audio-source/drumwah.mp3',
-                    style = '../ai-music-viz/cached-pkls/stylegan2/cat.pkl')
+L = LucidSonicDream(song = '../../../ai-music-viz/audio-source/drumwah.mp3',
+                    style = '../../../ai-music-viz/cached-pkls/stylegan2/cat.pkl')
 
-L.hallucinate(file_name = '../ai-music-viz/song-renders/drumwah-cats.mp4',
+L.hallucinate(file_name = '../../../ai-music-viz/song-renders/drumwah-cats.mp4',
              fps = 60, # Actual video FPS. Was: fps=self.sr/self.frame_duration. frame_dur=int(sr/fps - (sr/fps % 64)) (sr = sample rate)
-             speed_fpm = 12,
+             speed_fpm = 7,
              truncation = 0.4, # Between 0 and 1
 
-             pulse_react = 0.62,
+             pulse_react = 0.32,
              pulse_percussive = True, # If True while pulse_harmonic is False, pulse reacts to the audio's percussive elements.
              pulse_harmonic = False, # If True while pulse_percussive is False, pulse reacts to the audio's harmonic elements
 
              motion_react = 0.42,
              motion_percussive = False,
              motion_harmonic = True,
-             motion_randomness = 0.42, # Between 0 and 1
+             motion_randomness = 0.12, # Between 0 and 1
 
              contrast_percussive = True,
              contrast_strength = 0.76,
