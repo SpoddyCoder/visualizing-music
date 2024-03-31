04a190071e830fb20a56911ddc9e1a88557215b5
SpoddyCoder <paul@spoddycoder.com>
2024-03-31 03:44:41 +0100

    fix path ref after rejig

--- lucid-sonic-dreams/drumwah.py
+++ lucid-sonic-dreams/drumwah.py
@@ -1,9 +1,9 @@
 from lucidsonicdreams import LucidSonicDream
 
-L = LucidSonicDream(song = '../../../ai-music-viz/audio-source/drumwah.mp3',
-                    style = '../../../ai-music-viz/cached-pkls/stylegan2/cat.pkl')
+L = LucidSonicDream(song = '../../ai-music-viz/audio-source/drumwah.mp3',
+                    style = '../../ai-music-viz/cached-pkls/stylegan2/cat.pkl')
 
-L.hallucinate(file_name = '../../../ai-music-viz/song-renders/drumwah-cats.mp4',
+L.hallucinate(file_name = '../../ai-music-viz/song-renders/drumwah-cats.mp4',
              fps = 60, # Actual video FPS. Was: fps=self.sr/self.frame_duration. frame_dur=int(sr/fps - (sr/fps % 64)) (sr = sample rate)
              speed_fpm = 7,
              truncation = 0.4, # Between 0 and 1
@@ -18,7 +18,7 @@ L.hallucinate(file_name = '../../../ai-music-viz/song-renders/drumwah-cats.mp4',
              motion_randomness = 0.12, # Between 0 and 1
 
              contrast_percussive = True,
-             contrast_strength = 0.76,
+             contrast_strength = 0.26,
 
              flash_percussive = False,
              flash_strength = 0.05,
